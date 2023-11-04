c36d6fcfd77d748339caf5c42348cd4913a39e79
Nicolas Aguilera Garcia <niagar25@gmail.com>
2023-11-04 13:09:32 -0500

    [ADD] Init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cereal/archives/json.hpp>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    auto videoMode = sf::VideoMode(200, 300);
+    auto window = new sf::RenderWindow(videoMode, "CPP Board");
+
+    while (window->isOpen()) {
+        sf::Event event{};
+        while (window->pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+    }
+}
